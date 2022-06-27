954fe7ff8d51c37cbc816ebbf75a02ee447375c7
Suraiya Aysha <suraiyaaysha.cse@gmail.com>
2022-06-27 20:18:27 +0600

    create Todo Components

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
